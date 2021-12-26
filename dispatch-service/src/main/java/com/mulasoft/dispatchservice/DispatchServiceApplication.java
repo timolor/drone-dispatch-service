3f6e252be73f35d17d72411d1ed7b18c31f9330b
Oloruntoba Timilehin <timolor94@gmail.com>
2021-12-26 11:50:17 +0100

    Initial Commit

---
+++ dispatch-service/src/main/java/com/mulasoft/dispatchservice/DispatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mulasoft.dispatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchServiceApplication.class, args);
+	}
+
+}
---
+++ dispatch-service/src/test/java/com/mulasoft/dispatchservice/DispatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulasoft.dispatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
